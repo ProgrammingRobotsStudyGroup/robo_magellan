cbd7cabf90a1e1e1e9704d815628bd969fc6e814
jmdahling <jmdahling@gmail.com>
2017-02-09 12:12:26 -0800

    Clean up script

--- victoria.sh
+++ victoria.sh
@@ -3,23 +3,47 @@
 # Check out and build code for Victoria
 #
 # Assumes catkin workspace ~/catkin_ws
-# Assumes ROS Kinetic
-# Any assumptions for cone finder?
+# Assumes ROS Kinetic and MAVROS installed
 #
-set -x #echo on
+source ~/catkin_ws/devel/setup.bash
+#set -x #echo on
 
-# Fetch and mvoe to the right place the cone finder
+############################################################################
+echo
+echo Fetch/update robo_magellan
+
+if rospack list | grep "robo_magellan" >/dev/null
+then
+echo Update robo_magellan
+roscd robo_magellan
+git pull
+
+else
+echo Fetch robo_magellan
+cd ~/catkin_ws/src
+git clone https://github.com/ProgrammingRobotsStudyGroup/robo_magellan.git
+fi 
+
+
+############################################################################
+echo
+echo Fetch/update cone_finder
+
+if rospack list | grep "cone_finder" >/dev/null
+then
+echo Update cone_finder
+roscd cone_finder
+git pull
+
+else
+echo Fetch cone_finder
 cd ~/catkin_ws/src
 git clone https://github.com/ProgrammingRobotsStudyGroup/TrafficConeFinderCode.git
+fi 
 
-# Fetch MAVROS
-# if MavROS exists, skip install
-#
-#rospack list|grep
 
-# Build
+############################################################################
+echo Build
 cd ~/catkin_ws
 catkin_make
-
-
 
