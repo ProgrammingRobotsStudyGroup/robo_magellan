5ab1209c0b2b974228dda55862a0e474ac19dbdf
javatechs@gmail.com <javatechs@gmail.com>
2017-02-20 11:05:25 -0800

    Report MAV and ARM modes

--- scripts/monitor.py
+++ scripts/monitor.py
@@ -5,34 +5,54 @@ import rospkg
 
 from std_msgs.msg import String
 
+import uav_state
+
 # Enum maps to sound name
 import soundmap
 
 def monitor():
+    rospy.init_node('monitor', anonymous=True)
+
+    UAV = uav_state.UAV_State()
+
     pubFile  = rospy.Publisher('playfile', String, queue_size=10)
     pubToken = rospy.Publisher('play', String, queue_size=10)
-    rospy.init_node('monitor', anonymous=True)
-    rate = rospy.Rate(0.3) # some hz
+    rate = rospy.Rate(0.5) # some hz
+
     # Get an instance of RosPack with the default search paths
     rospack = rospkg.RosPack()
+
     # Get the file path for rospy_tutorials
     basepath = rospack.get_path('robo_magellan') + "/scripts/sounds/"
-    #basepath = '~/catkin_ws/src/robo_magellan/scripts/sounds/'
 
+    # TODO Are we connected to px? Is MAVROS up?
+
+    # state
     play_file = False
+    last_mode = None
+    last_arm = None
+
     while not rospy.is_shutdown():
-        # TODO Are we connected to px? Is MAVROS up?
-        # TODO Get MAV mode
-        # TODO Test code. Play 1) a sound ref'd by token name or 2) file path.
-        if play_file:
-            soundfile = basepath + soundmap.SoundMapEnum.ARMED.file_name
-            rospy.loginfo(soundfile)
-            pubFile.publish(soundfile)
-        else:
-            soundToken =  soundmap.SoundMapEnum.DISARMED.name
-            rospy.loginfo(soundToken)
-            pubToken.publish(soundToken)
-        play_file = not play_file
+        #
+        this_mode = str(UAV.get_mode())
+        if this_mode != last_mode:
+            pubToken.publish(this_mode)
+            rospy.loginfo(this_mode)
+        last_mode = this_mode
+
+        #
+        this_arm = UAV.get_arm()
+        if this_arm != last_arm:
+            token = soundmap.SoundMapEnum.ARMED.name
+            if this_arm:
+                token = soundmap.SoundMapEnum.ARMED.name
+            else:
+                token = soundmap.SoundMapEnum.DISARMED.name
+            pubToken.publish(token)
+            rospy.loginfo('Arm state: '+token)
+        last_arm = this_arm
+
+        #
         rate.sleep()
 
 if __name__ == '__main__':
