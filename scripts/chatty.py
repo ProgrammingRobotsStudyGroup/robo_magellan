d6164394db34a4a26904db1aecc27293b1b62396
javatechs@gmail.com <javatechs@gmail.com>
2017-02-20 10:58:59 -0800

    Refactor
    Use common exec logic.
    Create basepath globally.

--- scripts/chatty.py
+++ scripts/chatty.py
@@ -42,30 +42,37 @@ from std_msgs.msg import String
 import os
 import commands
 import soundmap
+import subprocess as subp
+
+basepath = None
+
+def exec_cmd(command):
+    # Sound sometimes doesn't play. Add padding
+    shh_cmd = 'paplay ' + basepath + '/scripts/sounds/' + 'silence-1sec.wav'
+    process = subp.Popen(shh_cmd, shell=True)
+    # Play file
+    process = subp.Popen(command, shell=True)
+    process.wait()
+    #os.system(command)
+    # Sound sometimes doesn't play. Add padding
+    shh_cmd = 'paplay ' + basepath + '/scripts/sounds/' + 'silence-1sec.wav'
+    process = subp.Popen(shh_cmd, shell=True)
 
 def callback_file(data):
     rospy.loginfo(rospy.get_caller_id() + ' Play file: %s', data.data)
-    os.system('paplay '+data.data)
-    #x = commands.getoutput(cmd)
-    #rospy.loginfo(rospy.get_caller_id() + 'Result: ', x)
+    cmd = 'paplay '+data.data
+    exec_cmd(cmd)
 
 def callback_token(data):
     rospy.loginfo(rospy.get_caller_id() + ' Play token: %s', data.data)
     # Convert token to file name
     try:
-        #   Get an instance of RosPack with the default search paths
-        rospack = rospkg.RosPack()
-        # get the file path for rospy_tutorials
-        basepath = rospack.get_path('robo_magellan')
         # Find the enum by token, create path, play
         fn = soundmap.SoundMapEnum.__getattr__(data.data).file_name
         path = basepath + '/scripts/sounds/' + fn
-        print(path)
-        os.system('paplay '+path)
-        #obj = soundmap.SoundMapEnum._member_map_[data.data]
-        #rospy.loginfo(obj)
-        #rospy.loginfo(obj.file_name)
-        #os.system('paplay '+obj.file_name)
+        rospy.loginfo('Path: '+path)
+        cline = 'paplay '+path
+        exec_cmd(cline)
     except:
         rospy.loginfo('No enum named: '+data.data)
 
@@ -78,6 +85,12 @@ def chatty():
     # name for our 'listener' node so that multiple listeners can
     # run simultaneously.
     rospy.init_node('chatty', anonymous=True)
+
+    # Get an instance of RosPack with the default search paths
+    rospack = rospkg.RosPack()
+    # Get file path of this package
+    global basepath 
+    basepath = rospack.get_path('robo_magellan')
 
     rospy.Subscriber('playfile', String, callback_file)
     rospy.Subscriber('play', String, callback_token)
