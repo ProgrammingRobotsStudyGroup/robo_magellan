9338ce6dcb8b0f7ea38ce6e22c07296c7637fe87
javatechs@gmail.com <javatechs@gmail.com>
2017-02-21 16:05:30 -0800

    Bad character

--- scripts/shutdownSwitch.py
+++ scripts/shutdownSwitch.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python#
+#!/usr/bin/env python
 #This script was authored by AndrewH7 and belongs to him (www.instructables.com/member/AndrewH7)
 #You have permission to modify and use this script only for your own personal usage
 #You do not have permission to redistribute this script as your own work
